814e5c635b68f214c4dedde85053ea672ae387ec
Swapnil Vishwakarma <swapnil.vishwakarma18@siesgst.ac.in>
2021-02-16 15:14:50 +0530

    Const, let

---
+++ 31.Shape matcher game/script.js
@@ -0,0 +1,30 @@
+let currentScore = 0;
+
+// if (currentScore < 5) {
+//     let currentScore = 10;
+//     console.log(currentScore + ' inside')
+// }
+
+// console.log(currentScore + ' outside')
+
+let playing = false;
+let shape1;
+let shape2;
+
+const shapes = [
+    {color: '#FF595E', width: 250, height: 160},
+    {color: '#FF595E', width: 270, height: 150},
+    {color: '#FFCA3A', width: 320, height: 170},
+    {color: '#FFCA3A', width: 310, height: 180},
+    {color: '#8AC926', width: 190, height: 160},
+    {color: '#8AC926', width: 200, height: 175},
+    {color: '#1982C4', width: 380, height: 185},
+    {color: '#1982C4', width: 400, height: 120},
+    {color: '#6A4C93', width: 370, height: 145},
+    {color: '#6A4C93', width: 440, height: 160},
+]
+
+console.log(shapes[2])
+
+
+// let is similar to var. It has block scopingA
